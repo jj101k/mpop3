d44dbee548bbe43e433f021ac9deaca9b31b9bb5
Jim (encrypted) <jimcrypt@Jim-Driscolls-Computer.local>
2006-01-15 13:17:46 +0000

    Use git tar-tree: it's easier

--- mktar.sh
+++ mktar.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 progname=`basename $PWD`
 full_vername=$progname-`git describe --tags | sed s/^v//`
-cd ..
-cp -RP $progname $full_vername
-tar czvf $full_vername.tar.gz --exclude '*/.*' $full_vername
-rm -rf $full_vername
+git tar-tree HEAD $full_vername | gzip -c > ../$full_vername.tar.gz
