1fc30e1f1c4f898e34574e667619b1bf28f5b703
Jim (encrypted) <jimcrypt@Jim-Driscolls-Computer.local>
2006-01-15 12:58:42 +0000

    Initial revision

---
+++ mktar.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+progname=`basename $PWD`
+full_vername=$progname-`git name-rev --tags HEAD| awk '{print $2}' | sed s/^v//`
+cd ..
+cp -RP $progname $full_vername
+tar czvf $full_vername.tar.gz --exclude '*/.*' $full_vername
+rm -rf $full_vername
